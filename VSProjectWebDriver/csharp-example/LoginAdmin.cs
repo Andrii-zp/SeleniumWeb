5a7f93050deeb8059aad80321b7e404ecf34e604
Andrii <terentievand@gmail.com>
2020-04-22 23:03:19 +0300

    AddVSProject

---
+++ VSProjectWebDriver/csharp-example/LoginAdmin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class Login
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void LoginAdmin()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            //driver.FindElement(By.Name("btnG")).Click(); + Keys.Enter
+            //wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ VSProjectWebDriver/csharp-example/OpenBrowser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Enter);
+            //driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
